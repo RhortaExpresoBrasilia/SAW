6b6a43070e409d85e821cdb947da57dc6826b2c0
EXPRESOBRASILIA\rhorta <rhorta@expresobrasilia.com>
2024-09-11 15:34:58 -0500

    ajustes de cargue masivo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,9 @@ export const environment = {
   production: true,
   url: 'https://webservices.expresobrasilia.co/BrasiliaMipresWS',
   urlV1: 'https://webservices.expresobrasilia.co/BrasiliaMipresWS/api/v1',
-  urlV2: 'https://webservices.expresobrasilia.co/BrasiliaOperacionesWS/api/v1',
+  urlV2: 'https://webservices.expresobrasilia.co/BrasiliaMipresWS/api/v1',
    //Operaciones
-  urlV3: 'https://webservices.expresobrasilia.co/BrasiliaOperacionesWS',
+  urlV3: 'https://webservices.expresobrasilia.co/BrasiliaMipresWS',
   urlReport: 'https://webservices.expresobrasilia.co/ReportesPW/resources/GWTJReportGenerator.html?cn=com.brasilia.tiquetepw.reportes.REP_TIQW001&createPl=T'
 
 };
